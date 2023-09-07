33effa914c4c933d1c0464ce5605208372837f76
Mertcan Yasar <canyasar924@gmail.com>
2023-09-08 01:03:21 +0300

    works

---
+++ Day28-29/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./"],
+  theme: {
+    extend: {
+      colors:{
+        yeniRenk: '#1F2937',
+        yeniRenk2: '#E5E7EB'
+      }
+    },
+  },
+  plugins: [],
+}
+
